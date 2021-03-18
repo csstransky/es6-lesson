a248dfa528dcad5272bedfbd7fcc4e0057bf3568
cstransky <csstransky@gmail.com>
2021-03-18 11:10:33 -0400

    Chapter 2 Lesson 3

---
+++ Exercise Files/Ch02/02_03/start/script.js
@@ -0,0 +1,14 @@
+function print(firstName) {
+    console.log("Hello", firstName);
+    console.log(`Hello ${firstName}`);
+}
+
+function createEmail(firstName, price) {
+    let shipping = 5.95;
+    console.log(`Hi ${firstName}! Thanks!
+        Total: $${price}
+        Shipping: $${shipping}
+        Grand Total: $${price + shipping}`)
+}
+print("tom");
+createEmail("Guy", 15);
